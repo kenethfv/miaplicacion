4de299098b388a1ee21a14f1f251eb78e4067e0d
Keneth Vasquez <kenethfvm.13@gmail.com>
2021-03-27 09:59:16 -0600

    pruebas

---
+++ src/app/ejemplo2/ejemplo2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo2',
+  templateUrl: './ejemplo2.component.html',
+  styleUrls: ['./ejemplo2.component.scss']
+})
+export class Ejemplo2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
